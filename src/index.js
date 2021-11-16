4efe2edc58d81e6d133d55b3245036c1bd499ffe
Fredys Fernandez <juniorfrey2990@gmail.com>
2021-11-16 11:09:40 -0500

    Se valida la recarga de la pagina

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { router } from './router/index.routes';
 import './style.css';
 
-router('#/');
+//* Cuando se ingresa a la pagina prinicpal y cuando se recarga la pagina por url
+if(window.location.hash == ''){
+    router('#/');
+}else{
+    router(window.location.hash);
+}
+
 window.addEventListener('hashchange', () => {
     router(window.location.hash);
 });
